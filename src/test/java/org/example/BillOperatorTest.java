7019346594e0b2fc095a8da86c1e62f5eca81432
jiggensnaggen <nickallee8529@gmail.com>
2022-05-20 12:23:24 -0700

    more work on testing the prompt user for manual bill instance

--- src/test/java/org/example/BillOperatorTest.java
+++ src/test/java/org/example/BillOperatorTest.java
@@ -2,8 +2,9 @@ package org.example;
 
 import Operators.BillOperator;
 import org.apache.log4j.Logger;
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -12,12 +13,15 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BillOperatorTest {
     Logger testLogger = Logger.getRootLogger();
     BillOperator testBillOperator = new BillOperator(testLogger);
 
+    @Rule
+    public final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
 
     public void testPromptUserForSelection(){
     }
@@ -57,11 +61,14 @@ class BillOperatorTest {
         Scanner testScanner = new Scanner(System.in);
         String[] passingBillInput = {"1", "carls jr", "24.86", "card", "navy fed main"};
         List<String> expected = List.of(passingBillInput);
-        BillOperator obj = new BillOperator(testLogger);
-        String userInput ="1, carls jr, 24.86, card, navy fed main";
 
-        ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
-        System.setIn(bais);
+
+        String userInput ="1, carls jr, 24.86, card, navy fed main";
+        systemInMock.provideLines(userInput);
+
+        BillOperator obj = new BillOperator(testLogger);
+
+        List<String> value = obj.promptUserForManualBillInstance(testScanner);
 
 //        ByteArrayOutputStream baos = new ByteArrayOutputStream();
 //        PrintStream printStream = new PrintStream(baos);
@@ -70,6 +77,6 @@ class BillOperatorTest {
 
 
 
-        assertEquals(expected,obj.promptUserForManualBillInstance(testScanner));
+        assertEquals(expected,value);
     }
 }
